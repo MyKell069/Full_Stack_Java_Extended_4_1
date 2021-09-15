df23c59658d2118f22c7ff6e41202156a5a33e0e
Kyle <Kyle.Plummer@revature.com>
2021-09-15 13:11:28 -0400

    breaking for lunch, static to do list

--- to-do-app/src/main/java/Driver.java
+++ to-do-app/src/main/java/Driver.java
@@ -1,3 +1,9 @@
+import models.ToDoItem;
+import utils.PrintView;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Driver {
     public static void main(String[] args) {
 
@@ -6,7 +12,20 @@ public class Driver {
             System.out.println(args[i]);
         }
 
+        List<ToDoItem> toDoList = new LinkedList<>();
 
+        ToDoItem newItem1 = new ToDoItem("Build a to do list");
+        toDoList.add(newItem1);
+
+        ToDoItem newItem2 = new ToDoItem("Debug the new to do list");
+        toDoList.add(newItem2);
+
+        ToDoItem newItem3 = new ToDoItem("enjoy your new to do list!");
+        toDoList.add(newItem3);
+
+        for(int i = 0; i < toDoList.size(); i++) {
+            PrintView.printMyView(i + ") " + toDoList.get(i).getToDoText());
+        }
 
 
     }
