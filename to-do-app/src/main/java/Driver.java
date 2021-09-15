8267a7365c99f8598e90db36e6da6322194ad718
Kyle <Kyle.Plummer@revature.com>
2021-09-15 12:44:20 -0400

    adding some files, testing new branch

---
+++ to-do-app/src/main/java/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    public static void main(String[] args) {
+
+        // View command line arguments passed to app
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+
+
+
+    }
+}
