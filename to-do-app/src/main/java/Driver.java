f5f7b70e47d16a69e8b76d926d20ab513eb22dff
Kyle <Kyle.Plummer@revature.com>
2021-10-19 09:18:56 -0400

    devops stuff

---
+++ to-do-app/src/main/java/Driver.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Driver {
+    public static void main(String[] args) {
+
+
+        TestClass obj = new TestClass();
+
+        System.out.println(obj.getY());
+
+
+    }
+}
---
+++ to-do-app/src/main/java/TestClass.java
@@ -0,0 +1,13 @@
+public class TestClass {
+    public static int x = 5;
+    public int y = 10;
+
+    public static int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
